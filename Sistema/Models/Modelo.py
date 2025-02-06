50f43dabe836c782b15fdc5945f7406129310d58
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-02-06 11:16:02 +0000

    Criação do Usuário | Criação da Herança de Modelos

---
+++ Sistema/Models/Modelo.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+class Modelo(ABC):
+    objetos = []
+
+    @classmethod
+    def inserir(cls, obj: object):
+        cls.abrir()
+        id = 0
+        for objeto in cls.objetos:
+            if objeto.getId():
+                id = objeto.getId()
+        obj.setId(id+1)
+        cls.objetos.append(obj)
+        cls.salvar()
+
+    @classmethod
+    def abrir():
+        pass
+
+    @classmethod
+    def salvar():
+        pass
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        return cls.objetos
+
+    @classmethod
+    def listarId(cls):
+        cls.abrir()
+        for i in cls.objetos:
+            if i.getId() == id:
+                return i
+        return None
+    
+    @classmethod
+    def atualizar(cls, obj : object):
+        x = cls.listarId(obj.getId())
+        if x != None:
+            cls.objetos.remove(x)
+            cls.objetos.append(obj)
+            cls.salvar()
+
+    @classmethod
+    def excluir(cls, obj:object):
+        x = cls.listarId(obj.getId())
+        if x != None:
+            cls.objetos.remove(x)
+            cls.salvar()
--- Sistema/Models/Usuário.py
+++ Sistema/Models/Usuário.py
@@ -1,7 +1,13 @@
+import json
+from Models.Modelo import Modelo
+
 class Usuário:
     def __init__(self, id, admin, nome, cpf, telefone):
         self.__id = id
         self.__admin = admin
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__telefone = telefone
 
     def getId(self):
         return self.__id
@@ -13,5 +19,40 @@ class Usuário:
     def setAdmin(self, admin):
         return self.__admin == admin
     
+    def getNome(self):
+        return self.__nome
+    def setNome(self, nome):
+        return self.__nome == nome
+    
+    def getCPF(self):
+        return self.__cpf
+    def setCPF(self, cpf):
+        return self.__cpf == cpf
+
+    def getTelefone(self):
+        return self.__telefone
+    def setTelefone(self, telefone):
+        return self.__telefone == telefone
+    
     def __str__(self):
-        return f"Id: {self.__id} - Admin: {self.__admin}"
+        return f"Id: {self.__getId()} - Nome: {self.__getNome()} - Cpf: {self.__getCPF()} - Telefone {self.__getTelefone()}"
+
+class Usuários(Modelo):
+    @classmethod
+    def abrir(cls):
+        cls.objetos = []
+        try:
+            with open ("Sistema/Json/Usuários.json", mode="r") as arquivo:
+                objetos_json = json.load(arquivo)
+                for obj in objetos_json:
+                    u = Usuário(obj["_Usuário_id"], obj["_Usuário_admin"], obj["_Usuário_nome"],  obj["_Usuário_cpf"],  obj["_Usuário_telefone"])
+                    cls.objetos.append(u)
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            pass
+
+    @classmethod
+    def salvar(cls):
+        with open("Sistema/Json/Usuários.json", mode="w") as arquivo:
+            json.dump(cls.objetos, arquivo, default=vars)
